11a3c15156b6919a1b3e480763f1845dd611b79e
Edward Yu <yedward@vmware.com>
2018-05-11 16:07:50 -0400

    bump version to 3.3.3

--- www/js/crittercism.js
+++ www/js/crittercism.js
@@ -1,18 +1,18 @@
 // Copyright 2014 Crittercism
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var CRITTERCISM_VERSION = "v3.3.2";
+var CRITTERCISM_VERSION = "v3.3.3";
 
 var exec = require("cordova/exec");
 
@@ -27,7 +27,7 @@ var	Crittercism = {
         cordova.exec(success, fail, "CDVCrittercism", "crittercismInit", [appID]);
         return this;
     },
-    
+
     leaveBreadcrumb: function(breadCrumb) {
         cordova.exec(success, fail, "CDVCrittercism", "crittercismLeaveBreadcrumb", [breadCrumb]);
         return this;
